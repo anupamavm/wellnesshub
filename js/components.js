8812b8c7ca8392b642e0d3c1bd60c6460c1c2555
Anupama Morapitiya <anupamamorapitiya97@gmail.com>
2024-12-30 11:33:03 +0530

    added contact form validations

--- js/components.js
+++ js/components.js
@@ -1,26 +1,26 @@
 async function loadComponent(componentPath, targetId) {
-	try {
-		const response = await fetch(componentPath);
-		if (response.ok) {
-			const content = await response.text();
-			document.getElementById(targetId).innerHTML = content;
-		} else {
-			console.error(`Error loading component: ${componentPath}`);
-		}
-	} catch (error) {
-		console.error(`Error: ${error}`);
-	}
+  try {
+    const response = await fetch(componentPath);
+    if (response.ok) {
+      const content = await response.text();
+      document.getElementById(targetId).innerHTML = content;
+    } else {
+      console.error(`Error loading component: ${componentPath}`);
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
 }
 
 // Function to load pages dynamically
 async function loadPage(pageName) {
-	const pagePath = `pages/${pageName}.html`;
-	await loadComponent(pagePath, "page-content");
+  const pagePath = `pages/${pageName}.html`;
+  await loadComponent(pagePath, "page-content");
 }
 
 // Initialize header, footer, and default page
 document.addEventListener("DOMContentLoaded", () => {
-	loadComponent("components/header.html", "header");
-	loadComponent("components/footer.html", "footer");
-	loadPage("home"); // Default page is 'home'
+  loadComponent("components/header.html", "header");
+  loadComponent("components/footer.html", "footer");
+  loadPage("home");
 });
